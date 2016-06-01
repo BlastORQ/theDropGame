4ad1147a2d5b5b8d7784d893824b5ad952fb0dbf
senjka4 <senjka4@gmail.com>
2016-06-01 20:22:06 +0300

    Почав проект

---
+++ desktop/src/ua/pp/blastorq/drop/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package ua.pp.blastorq.drop.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import ua.pp.blastorq.drop.Drop;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Drop(), config);
+	}
+}
