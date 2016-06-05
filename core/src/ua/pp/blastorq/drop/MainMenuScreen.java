050cab4164f0517c88b53f9e1ae8ccf145f9e1d7
Manzhula David <da411d@gmail.com>
2016-06-05 22:44:35 +0300

    Зробив фон і гарний шрифт в MainMenu

--- core/src/ua/pp/blastorq/drop/MainMenuScreen.java
+++ core/src/ua/pp/blastorq/drop/MainMenuScreen.java
@@ -4,14 +4,19 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
 
 
 public class MainMenuScreen implements Screen {
 
     final Drop game;
     OrthographicCamera camera;
-
     Texture bg = new Texture("bg/mainmenu_bg.jpg");
+    BitmapFont font32 = new BitmapFont(Gdx.files.internal("fonts/BadScript-64.fnt"), false);
+    BitmapFont font48 = new BitmapFont(Gdx.files.internal("fonts/BadScript-64.fnt"), false);
+    BitmapFont font64 = new BitmapFont(Gdx.files.internal("fonts/BadScript-64.fnt"), false);
+    BitmapFont font128 = new BitmapFont(Gdx.files.internal("fonts/BadScript-128.fnt"), false);
 
     public MainMenuScreen(final Drop gam) {
         game = gam;
@@ -34,13 +39,32 @@ public class MainMenuScreen implements Screen {
 
         game.batch.setProjectionMatrix(camera.combined);
         game.batch.begin();
-        game.font.draw(game.batch, "Welcome to Drop!", 100, 150);
-        game.font.draw(game.batch, "Tap anywhere to begin!", 100, 100);
         game.batch.draw(bg, 0, 0);
+        //Чорна грань текста
+        font64.setColor(0.3f, 0.5f, 0.3f, 1);
+        font128.setColor(0.3f, 0.5f, 0.3f, 1);
+
+        font128.draw(game.batch, "Вітаємо в theDropGame!", 100+1, 300+1);
+        font128.draw(game.batch, "Вітаємо в theDropGame!", 100+1, 300-1);
+        font128.draw(game.batch, "Вітаємо в theDropGame!", 100-1, 300+1);
+        font128.draw(game.batch, "Вітаємо в theDropGame!", 100-1, 300-1);
+
+        font64.draw(game.batch, "Клікни де-небудь щоб почати!", 175+1, 200+1);
+        font64.draw(game.batch, "Клікни де-небудь щоб почати!", 175+1, 200-1);
+        font64.draw(game.batch, "Клікни де-небудь щоб почати!", 175-1, 200+1);
+        font64.draw(game.batch, "Клікни де-небудь щоб почати!", 175-1, 200-1);
+
+        //А тепер сам текст
+        font64.setColor(1, 1, 1, 1);
+        font128.setColor(1, 1, 1, 1);
+        font128.draw(game.batch, "Вітаємо в theDropGame!", 100, 300);
+        font64.draw(game.batch, "Клікни де-небудь щоб почати!", 175, 200);
+
+
         game.batch.end();
 
         if (Gdx.input.isTouched()){
-            game.setScreen(new StartLoading(game, new GameScreen(game)));
+            game.setScreen(new GameScreen(game));
             dispose();
         }
 
