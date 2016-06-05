724198985204503e333c83ef4791fb3bed618cd8
Manzhula David <da411d@gmail.com>
2016-06-05 22:42:15 +0300

    Зробив гарний шрифт на екрані GameOver

--- core/src/ua/pp/blastorq/drop/GameOver.java
+++ core/src/ua/pp/blastorq/drop/GameOver.java
@@ -5,6 +5,8 @@ import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.math.MathUtils;
 import com.badlogic.gdx.math.Vector3;
 
 import java.awt.Rectangle;
@@ -19,9 +21,13 @@ public  class GameOver implements Screen {
     final Drop game;
     boolean isPaused;
     OrthographicCamera camera;
-    Texture bg = new Texture("bg/gameover_bg.jpg");
     int canStartAgain = 100;
     int score , highscore;
+    Texture bg = new Texture("bg/gameover_bg.jpg");
+    BitmapFont font32 = new BitmapFont(Gdx.files.internal("fonts/BadScript-64.fnt"), false);
+    BitmapFont font48 = new BitmapFont(Gdx.files.internal("fonts/BadScript-64.fnt"), false);
+    BitmapFont font64 = new BitmapFont(Gdx.files.internal("fonts/BadScript-64.fnt"), false);
+    BitmapFont font128 = new BitmapFont(Gdx.files.internal("fonts/BadScript-128.fnt"), false);
 
 
     public GameOver(final Drop gam, int scor , int highscor) {
@@ -46,16 +52,38 @@ public  class GameOver implements Screen {
 
         game.batch.setProjectionMatrix(camera.combined);
         game.batch.begin();
-        game.font.draw(game.batch, "Game over!", 100, 150);
-        game.font.draw(game.batch, "Your score is "+score, 100, 125);
-        game.font.draw(game.batch, "Your highscore is "+highscore, 100, 115);
         game.batch.draw(bg, 0, 0);
 
-        game.font.draw(game.batch, "Tap anywhere to try again", 100, 100);
+        //Чорна грань текста
+        font64.setColor(0.3f, 0.3f, 0.5f, 1);
+        font128.setColor(0.3f, 0.3f, 0.5f, 1);
+
+        font128.draw(game.batch, "Game over!", 260+1, 320+1);
+        font128.draw(game.batch, "Game over!", 260+1, 320-1);
+        font128.draw(game.batch, "Game over!", 260-1, 320+1);
+        font128.draw(game.batch, "Game over!", 260-1, 320-1);
+
+        font64.draw(game.batch, "Твій рахунок - "+score, 275+1, 210+1);
+        font64.draw(game.batch, "Твій рахунок - "+score, 275+1, 210-1);
+        font64.draw(game.batch, "Твій рахунок - "+score, 275-1, 210+1);
+        font64.draw(game.batch, "Твій рахунок - "+score, 275-1, 210-1);
+
+        font64.draw(game.batch, "Клікни будь-де щоб спробувати знову", 175+1, 160+1);
+        font64.draw(game.batch, "Клікни будь-де щоб спробувати знову", 175+1, 160-1);
+        font64.draw(game.batch, "Клікни будь-де щоб спробувати знову", 175-1, 160+1);
+        font64.draw(game.batch, "Клікни будь-де щоб спробувати знову", 175-1, 160-1);
+
+        //А тепер сам текст
+        font64.setColor(1, 1, 1, 1);
+        font128.setColor(1, 1, 1, 1);
+        font128.draw(game.batch, "Game over!", 260, 320);
+        font64.draw(game.batch, "Твій рахунок - "+score, 275, 210);
+        font64.draw(game.batch, "Клікни будь-де щоб спробувати знову", 175, 160);
+
         game.batch.end();
 
         if (Gdx.input.isTouched() && canStartAgain<=0){
-            game.setScreen(new MainMenuScreen(game));
+            game.setScreen(new GameScreen(game));
             dispose();
         }else{
             canStartAgain--;
